9a2a7e375e455ea31109488321e5f14a228b9cb7
code3-dev <h3dev.pira@gamil.com>
2025-10-27 00:35:39 +0330

    Fix Linux 386 build by correcting build constraints for systray wrapper

--- internal/app/tray_wrapper.go
+++ internal/app/tray_wrapper.go
@@ -1,8 +1,10 @@
-//go:build windows || linux || darwin
+//go:build (windows && amd64) || (linux && amd64) || (darwin && (amd64 || arm64))
 
 package app
 
 // RunTrayWrapper calls the actual systray implementation
 func RunTrayWrapper() {
+	// This will call RunTray which is only available on supported platforms
+	// due to build constraints in tray.go
 	RunTray()
 }
